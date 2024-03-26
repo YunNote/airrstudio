549f13351884067e5803b75086dfb284aeffd226
YunNote <zzdd1558@gmail.com>
2024-03-27 00:08:39 +0900

    feature: 컨텐츠 영역 작업중

--- src/components/Portfolio/PortFolio.jsx
+++ src/components/Portfolio/PortFolio.jsx
@@ -1,10 +1,23 @@
 import styles from "@/components/Portfolio/PortFolio.module.css";
 
-export default function PortFolio({portfolio : {id, title, thumbnail}}) {
+export default function PortFolio({portfolio : {title, thumbnail, startDate, endDate}}) {
 
-  return <div>
-    <p>{id}</p>
-    <p>{title}</p>
-    <img src={thumbnail} alt="이미지"/>
+  return <div className={styles.container}>
+    <div className={styles.header}>
+      <div>
+        <p>{title}</p>
+      </div>
+
+      <div>
+        {startDate} - {endDate}
+      </div>
+    </div>
+    <div className={styles.thumbnailHover}>
+      <div className={styles.hoverWrap}>
+        <p>{title}</p>
+      </div>
+      <img src={thumbnail} alt="이미지"/>
+    </div>
+
   </div>
 }
